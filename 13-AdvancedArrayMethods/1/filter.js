12982128e4835ccde322f046d86c4adcadd72b61
AndrewSushi <AndrewSushi8@gmail.com>
2022-12-04 01:17:19 -0800

    Changed 1 and 2

--- 13-AdvancedArrayMethods/1/filter.js
+++ 13-AdvancedArrayMethods/1/filter.js
@@ -30,3 +30,34 @@ const containsVowel = function(word){
 const noVowel = words.filter(function(word){
     return !containsVowel(word);
 })
+
+// const checked = allChecks.filter(function(box){
+//     return box.checked;
+// })
+
+// const checkedText = checked.map(function(checkbox){
+//     return checkbox.parentElement.innerText;
+// })
+
+function extractedCompletedTodos(){
+    const allChecks = Array.from(document.querySelectorAll('input[type="checkbox"]'));
+    return allChecks.filter(function(box){
+        return box.checked;
+    }).map(function(checkbox){
+        return checkbox.parentElement.innerText;
+    })
+}
+
+function myFilter(arr, callback){
+    const filteredArray = [];
+    for(let i = 0; i < arr.length; i++){
+        if(callback(arr[i], i, arr)){
+            filteredArray.push(arr[i]);
+        }
+    }
+    return filteredArray;
+}
+
+const longs = myFilter(words, function(word){
+    return word.length > 9;
+})
