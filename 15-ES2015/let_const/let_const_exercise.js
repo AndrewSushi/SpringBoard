02c827d3bec84d24e0415dc060352bceec72a9a6
AndrewSushi <AndrewSushi8@gmail.com>
2022-12-08 21:50:22 -0800

    Finished let_const

---
+++ 15-ES2015/let_const/let_const_exercise.js
@@ -0,0 +1,10 @@
+// var PI = 3.14;
+// PI = 42; // stop me from doing this
+
+/* Write an ES2015 Version */
+const PI = 3.14;
+
+// The difference between var and let is you can redeclare using var but not with let
+// With const you are unable to redeclare and reassign
+// You are able to reassign using let but not with const
+// Hoisting is when variables are declared at the top of the file as empty variables. You are not able to do this with const
