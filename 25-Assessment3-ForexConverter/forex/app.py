36a3f5e2b94d5823d3aa997797a6ff7583b8092d
AndrewSushi <AndrewSushi8@gmail.com>
2023-06-14 20:56:27 -0700

    Finished forex converter

--- 25-Assessment3-ForexConverter/forex/app.py
+++ 25-Assessment3-ForexConverter/forex/app.py
@@ -6,12 +6,6 @@ response = requests.get(url)
 data = response.json()
 currencies = data["rates"]
 
-# ?base=USD
-# print(data)
-# for currency, ammount in currencies:
-#     if currency == "USD":
-#         print(ammount)
-
 app = Flask(__name__)
 
 @app.route('/')
@@ -20,9 +14,22 @@ def home():
 
 @app.route('/convert')
 def convert():
-    from_currency = request.args["from"]
-    to_currency = request.args["to"]
-    amount = request.args["amount"]
-    # TODO type check and make sure they're normal
-    # TODO resend the api request with the from currency
-    return render_template('test.html', data=data)
+    from_currency = request.args.get("from")
+    to_currency = request.args.get("to")
+    amount = request.args.get("amount")
+
+    invalids = []
+    if not amount.isdigit():
+        invalids.append('amount.')
+    if from_currency is None or from_currency not in currencies:
+        invalids.append(f'code: {from_currency}')
+    if to_currency is None or to_currency not in currencies:
+        invalids.append(f'code: {to_currency}')
+    if invalids:
+        return render_template('home.html', invalids=invalids)
+    url = f'https://api.exchangerate.host/latest?base={from_currency}'
+    response = requests.get(url)
+    data = response.json()
+    rate = data["rates"][to_currency]
+    exchange = round(rate * int(amount), 2)
+    return render_template('currency.html', exchange=exchange, to_currency=to_currency)
