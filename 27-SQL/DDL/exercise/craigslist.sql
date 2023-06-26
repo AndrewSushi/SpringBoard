a8e94fb16bf4f3083267b718ca8bcc87a3d03aff
AndrewSushi <AndrewSushi8@gmail.com>
2023-06-25 18:10:41 -0700

    Finished Craigslist

--- 27-SQL/DDL/exercise/craigslist.sql
+++ 27-SQL/DDL/exercise/craigslist.sql
@@ -15,15 +15,17 @@ CREATE TABLE users(
     preferrend_region_id INT REFERENCES regions
 );
 
-CREATE TABLE posts(
-    id SERIAL PRIMARY KEY,
-    title VARCHAR(40) NOT NULL,
-    user_id INT REFERENCES users NOT NULL,
-    location_id INT REFERENCES regions NOT NULL,
-    region_id INT REFERENCES regions NOT NULL
-);
-
 CREATE TABLE categories(
     id SERIAL PRIMARY KEY,
     name VARCHAR(40)
+);
+
+CREATE TABLE posts(
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(40) NOT NULL,
+    description TEXT NOT NULL,
+    user_id INT REFERENCES users NOT NULL,
+    category_id INT REFERENCES categories NOT NULL,
+    location_id INT REFERENCES regions NOT NULL,
+    region_id INT REFERENCES regions NOT NULL
 );
