b05edb982f06cb45b7e60b9e600386170252c6f7
AndrewSushi <AndrewSushi8@gmail.com>
2023-08-04 22:00:53 -0700

    Changed app.js

--- 36-NodeFundamentals/static/app.js
+++ 36-NodeFundamentals/static/app.js
@@ -1,5 +1,30 @@
-console.log("Hello World")
+// let url = "https://swapi.dev/api/planets/1/"
+// axios.get(url)
+//     .then(res => {
+//         axios.get(res.data.residents[0])
+//             .then(res => {
+//                 console.log(res)
+//             })
+//             .catch(err => {
+//                 console.log("ERROR")
+//             })
+//     })
+//     .catch(err => console.log("REJECTED", err))
+
 let url = "https://swapi.dev/api/planets/1/"
-let ourFirstPromise = axios.get(url)
-ourFirstPromise.then(data => console.log(data))
-ourFirstPromise.catch(err => console.log("REJECTED", err))
+axios.get(url)
+    .then(res => {
+        console.log("First Request")
+        console.log(res.data)
+        return axios.get(res.data.residents[0])
+    })
+    .then(res => {
+        console.log("Second Request")
+        console.log(res.data)
+        return axios.get(res.data.films[0])
+    })
+    .then(res => {
+        console.log("Third Request")
+        console.log(res.data)
+    })
+    .catch(err => console.log("REJECTED", err))
