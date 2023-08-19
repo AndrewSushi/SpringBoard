1ec801da2fd309e1f8e57cfa54cd752791ff8b14
AndrewSushi <AndrewSushi8@gmail.com>
2023-08-18 20:16:45 -0700

    Finished the broken-app

--- 38-Assessment6-BrokenApp/broken-app/app.js
+++ 38-Assessment6-BrokenApp/broken-app/app.js
@@ -1,18 +1,36 @@
 const express = require('express');
-let axios = require('axios');
-var app = express();
+const axios = require('axios');
+const app = express();
+app.use(express.json());
 
-app.post('/', function(req, res, next) {
-  try {
-    let results = req.body.developers.map(async d => {
-      return await axios.get(`https://api.github.com/users/${d}`);
-    });
-    let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+app.post('/', async function(req, res, next) {
+    try {
+        let results = await Promise.all(req.body.developers.map(async d => {
+            return await axios.get(`https://api.github.com/users/${d}`);
+        }));
 
-    return res.send(JSON.stringify(out));
-  } catch {
-    next(err);
-  }
+        let out = results.map(r => ({ name: r.data.name, bio: r.data.bio }));
+
+        return res.send(JSON.stringify(out));
+    } catch (err) {
+        next(err);
+    }
 });
 
-app.listen(3000);
+app.use(function(err, req, res, next) {
+    console.error(err);
+    res.status(500).json({ error: 'Internal server error' });
+});
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
+
+// Errors:
+/*
+    Added app.use(express.json()) for middleware to parse the JSON requests
+    
+    Added a Promise.all() to make sure that all the requests are awaited
+
+    Added the app.use() middleware error catch instead of just a next(err)
+*/
