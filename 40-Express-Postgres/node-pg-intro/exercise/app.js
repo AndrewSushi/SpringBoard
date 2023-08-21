5438fe72a282703f2f1ac7cb4883ea45900c8b2d
AndrewSushi <AndrewSushi8@gmail.com>
2023-08-21 14:44:54 -0700

    Finished the exercise

--- 40-Express-Postgres/node-pg-intro/exercise/app.js
+++ 40-Express-Postgres/node-pg-intro/exercise/app.js
@@ -18,19 +18,19 @@ app.use("/invoices", invoicesRoutes);
 /** 404 handler */
 
 app.use(function (req, res, next) {
-  const err = new ExpressError("Not Found", 404);
-  return next(err);
+    const err = new ExpressError("Not Found", 404);
+    return next(err);
 });
 
 /** general error handler */
 
 app.use((err, req, res, next) => {
-  res.status(err.status || 500);
+    res.status(err.status || 500);
 
-  return res.json({
-    error: err,
-    message: err.message
-  });
+    return res.json({
+        error: err,
+        message: err.message
+    });
 });
 
 
--- 40-Express-Postgres/node-pg-intro/exercise/db.js
+++ 40-Express-Postgres/node-pg-intro/exercise/db.js
@@ -3,11 +3,12 @@
 
 const { Client } = require("pg");
 
-const client = new Client({
-  connectionString: "postgresql:///biztime"
+let db = new Client({
+    host: "/var/run/postgresql/",
+    database: "biztime"
 });
 
-client.connect();
+db.connect();
 
 
-module.exports = client;
+module.exports = db;
