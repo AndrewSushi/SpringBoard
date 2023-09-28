b8648cf3d314c00d713e7526a21ce5119b466275
AndrewSushi <AndrewSushi8@gmail.com>
2023-09-28 11:26:24 -0700

    Finished react state patterns circle buttons

--- 44-React/state-patterns/state-patterns/src/App.js
+++ 44-React/state-patterns/state-patterns/src/App.js
@@ -1,11 +1,17 @@
 import SimpleCounter from "./SimpleCounter"
 import NumbersList from "./NumbersList";
+import Circle from "./Circle";
+import ColoredCircles from "./ColoredCircles";
 
 function App() {
   return (
     <div className="App">
-      <NumbersList/>
-      <SimpleCounter/>
+      <ColoredCircles/>
+      {/* <Circle color="peachpuff" idx={8}/>
+      <Circle color="thistle" idx={10}/>
+      <Circle color="lavender" idx={2}/> */}
+      {/* <NumbersList/>
+      <SimpleCounter/> */}
 
     </div>
   );
