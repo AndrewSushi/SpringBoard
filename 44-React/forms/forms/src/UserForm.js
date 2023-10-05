1e2c1157871847e787c6d4212364ddced6e45ce0
AndrewSushi <AndrewSushi8@gmail.com>
2023-10-04 22:45:53 -0700

    Added forms

---
+++ 44-React/forms/forms/src/UserForm.js
@@ -0,0 +1,17 @@
+import React, {useState} from "react";
+
+const UserForm = () => {
+    const [username, setUsername] = useState("AndrewSushi")
+    const handleChange = (e) => {
+        setUsername(e.target.value)
+    }
+
+    return (
+        <form>
+            <input type="text" placeholder="username" value={username} onChange={handleChange}/>
+            <button>Add me to list!</button>
+        </form>
+    )
+}
+
+export default UserForm
