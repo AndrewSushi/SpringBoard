832e5e0a66bc2d99d3372a760437a478944d6b7f
AndrewSushi <AndrewSushi8@gmail.com>
2023-10-11 15:33:47 -0700

    Added timer useRef

--- 44-React/effects-refs/effects-ref/src/Timer.js
+++ 44-React/effects-refs/effects-ref/src/Timer.js
@@ -1,19 +1,27 @@
-import React, {useState, useEffect} from "react"
+import React, {useState, useEffect, useRef} from "react"
 
 const Timer = () => {
     const [seconds, setSeconds] = useState(0)
+    const timerId = useRef()
     useEffect(() => {
-        const intervalId = setInterval(() => {
+        timerId.current = setInterval(() => {
             setSeconds(seconds => seconds + 1)
         }, 1000);
     
         return () => {
-            clearInterval(intervalId);
+            clearInterval(timerId.current);
         };
     }, []);
     
+    const stopTimer = () => {
+        clearInterval(timerId.current)
+    }
+
     return (
-        <h1>{seconds}</h1>
+        <div>
+            <h1>{seconds}</h1>
+            <button onClick={stopTimer}>Stop</button>
+        </div>
     )
 }
 
