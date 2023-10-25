46c610cb00c14594a4e832af71db9ae6e508cb0d
AndrewSushi <AndrewSushi8@gmail.com>
2023-10-24 21:44:50 -0700

    Finished unroll

--- 47-Assessment9-SnackOrBooze/unroll/unroll.js
+++ 47-Assessment9-SnackOrBooze/unroll/unroll.js
@@ -1,5 +1,21 @@
 function unroll(squareArray) {
+    const unrolled = []
+    while(squareArray.length > 0){
+        unrolled.push(...squareArray.shift()) // Puts the first row into the unrolled array
 
+        for(let i = 0; i < squareArray.length; i++){ // Adds the last element in each row to the unrolled array
+            unrolled.push(squareArray[i].pop())
+        }
+
+        if(squareArray.length > 0){ // Adds the last row and reverses it
+            unrolled.push(...squareArray.pop().reverse())
+        }
+
+        for(let i = squareArray.length - 1; i >= 0; i--){ // Adds the first element of each row
+            unrolled.push(squareArray[i].shift())
+        }
+    }
+    return unrolled
 }
 
 module.exports = unroll;
--- 47-Assessment9-SnackOrBooze/unroll/unroll.test.js
+++ 47-Assessment9-SnackOrBooze/unroll/unroll.test.js
@@ -1,9 +1,61 @@
 const unroll = require("./unroll");
 
 describe("#unroll", function () {
-
   it("is a function", function () {
     expect(typeof unroll).toEqual("function");
   });
 
+  it("correctly unrolls a 2x2 square grid", function () {
+    const square = [
+      [1, 2],
+      [3, 4],
+    ];
+    const expected = [1, 2, 4, 3];
+    expect(unroll(square)).toEqual(expected);
+  });
+
+  it("correctly unrolls a 3x3 square grid", function () {
+    const square = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+    ];
+    const expected = [1, 2, 3, 6, 9, 8, 7, 4, 5];
+    expect(unroll(square)).toEqual(expected);
+  });
+
+  it("correctly unrolls a 4x4 square grid", function () {
+    const square = [
+      [1, 2, 3, 4],
+      [5, 6, 7, 8],
+      [9, 10, 11, 12],
+      [13, 14, 15, 16],
+    ];
+    const expected = [1, 2, 3, 4, 8, 12, 16, 15, 14, 13, 9, 5, 6, 7, 11, 10];
+    expect(unroll(square)).toEqual(expected);
+  });
+
+  it("correctly unrolls a 1x1 square grid", function () {
+    const square = [[1]];
+    const expected = [1];
+    expect(unroll(square)).toEqual(expected);
+  });
+
+  it("handles an empty square grid", function () {
+    const square = [];
+    const expected = [];
+    expect(unroll(square)).toEqual(expected);
+  });
+
+  it("handles a square grid with one row and multiple columns", function () {
+    const square = [[1, 2, 3, 4, 5]];
+    const expected = [1, 2, 3, 4, 5];
+    expect(unroll(square)).toEqual(expected);
+  });
+
+  it("handles a square grid with one column and multiple rows", function () {
+    const square = [[1], [2], [3], [4], [5]];
+    const expected = [1, 2, 3, 4, 5];
+    expect(unroll(square)).toEqual(expected);
+  });
 });
