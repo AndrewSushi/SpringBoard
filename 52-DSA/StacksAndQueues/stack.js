f423971e4cfe225cf5b10c589ec7a7632fbb5f4c
AndrewSushi <AndrewSushi8@gmail.com>
2023-10-26 23:13:14 -0700

    Finished stack exercise

--- 52-DSA/StacksAndQueues/stack.js
+++ 52-DSA/StacksAndQueues/stack.js
@@ -20,26 +20,47 @@ class Stack {
   /** push(val): add new value to end of the stack. Returns undefined. */
 
   push(val) {
-
+    let newNode = new Node(val)
+    if(!this.first){
+      this.first = newNode
+      this.last = newNode
+    }else{
+      newNode.next = this.first
+      this.first = newNode
+    }
+    this.size++
+    return undefined
   }
 
   /** pop(): remove the node from the top of the stack
    * and return its value. Should throw an error if the stack is empty. */
 
   pop() {
-
+    if(!this.first){
+      throw new Error("Stack is empty")
+    }
+    let val = this.first.val
+    this.first = this.first.next
+    this.size--
+    if(!this.size){
+      this.last = null
+    }
+    return val
   }
 
   /** peek(): return the value of the first node in the stack. */
 
   peek() {
-
+    if(!this.size){
+      throw new Error("Stack is empty")
+    }
+    return this.first.val
   }
 
   /** isEmpty(): return true if the stack is empty, otherwise false */
 
   isEmpty() {
-
+    return this.size == 0
   }
 }
 
