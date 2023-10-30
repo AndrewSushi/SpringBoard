fb685586436790233e964b7349ab6453656c94b4
AndrewSushi <AndrewSushi8@gmail.com>
2023-10-30 02:23:01 -0700

    Finished implementing insertion sort

--- 52-DSA/Sorting/insertion.js
+++ 52-DSA/Sorting/insertion.js
@@ -1,5 +1,15 @@
 function insertionSort(arr) {
-    
+    let notSorted = 1
+    while(notSorted){
+        notSorted = 0
+        for(let i = 1; i < arr.length; i++){
+            if(arr[i] < arr[i-1]){
+                [arr[i], arr[i - 1]] = [arr[i - 1], arr[i]]
+                notSorted = 1
+            }
+        }
+    }
+    return arr
 }
 
 module.exports = insertionSort;
