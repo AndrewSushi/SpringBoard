ad74ae75de5ecaa51887aaa782336e83b6d016eb
AndrewSushi <AndrewSushi8@gmail.com>
2023-11-09 10:43:19 -0800

    Did it using a Map as well

--- 53-InterviewPrep/ProblemSolvingPatterns/validAnagram.js
+++ 53-InterviewPrep/ProblemSolvingPatterns/validAnagram.js
@@ -1,7 +1,28 @@
+// function createMap(str) {
+//     const map = {}
+//     for (let char of str) {
+//         map[char] = (map[char] + 1) || 1
+//     }
+//     return map
+// }
+
+// function validAnagram(str1, str2) {
+//     if (str1.length !== str2.length) return false
+//     const str1Map = createMap(str1)
+//     const str2Map = createMap(str2)
+    
+//     for (let key in str1Map) {
+//         if (!str2Map[key]) return false
+//         if (str1Map[key] !== str2Map[key]) return false
+//     }
+//     return true
+// }
+
 function createMap(str) {
-    const map = {}
+    const map = new Map()
     for (let char of str) {
-        map[char] = (map[char] + 1) || 1
+        let charCount = map.get(char) || 0
+        map.set(char, charCount + 1)
     }
     return map
 }
@@ -11,9 +32,8 @@ function validAnagram(str1, str2) {
     const str1Map = createMap(str1)
     const str2Map = createMap(str2)
     
-    for (let key in str1Map) {
-        if (!str2Map[key]) return false
-        if (str1Map[key] !== str2Map[key]) return false
+    for (let letter of str1Map.keys()) {
+        if (str2Map.get(letter) !== str1Map.get(letter)) return false
     }
     return true
 }
