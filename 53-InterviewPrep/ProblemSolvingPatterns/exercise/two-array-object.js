5c7f19725e699ba04a6f9030d5412a41ab612a8c
AndrewSushi <AndrewSushi8@gmail.com>
2023-11-09 21:47:12 -0800

    Finished two array object

--- 53-InterviewPrep/ProblemSolvingPatterns/exercise/two-array-object.js
+++ 53-InterviewPrep/ProblemSolvingPatterns/exercise/two-array-object.js
@@ -1,3 +1,10 @@
 // add whatever parameters you deem necessary
-function twoArrayObject() {
+function twoArrayObject(arr1, arr2) {
+    let map = {}
+    for(let elementIdx in arr1){
+        map[arr1[elementIdx]] = arr2[elementIdx] || null
+    }
+    return map
 }
+
+module.exports = twoArrayObject
