bbddc5168b4e2d551e989fb24fbf06f05f928844
AndrewSushi <AndrewSushi8@gmail.com>
2023-11-09 22:25:12 -0800

    Finished longest fall

--- 53-InterviewPrep/ProblemSolvingPatterns/exercise/longest-fall.js
+++ 53-InterviewPrep/ProblemSolvingPatterns/exercise/longest-fall.js
@@ -1,3 +1,18 @@
 // add whatever parameters you deem necessary
-function longestFall() {
+function longestFall(arr) {
+    if(!arr.length)return 0
+    
+    let fall = 1
+    let longest = 1
+    for(let i = 0; i < arr.length - 1; i++){
+        if(arr[i] > arr[i + 1]){
+            fall++
+        }else{
+            longest = Math.max(longest, fall)
+            fall = 1
+        }
+    }
+    return Math.max(longest, fall)
 }
+
+module.exports = longestFall
