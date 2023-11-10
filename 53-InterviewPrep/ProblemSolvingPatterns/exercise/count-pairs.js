2455c9de6ac5a2289c7eca8c6a3ec552652ff8e5
AndrewSushi <AndrewSushi8@gmail.com>
2023-11-09 22:17:53 -0800

    Finished count pairs

--- 53-InterviewPrep/ProblemSolvingPatterns/exercise/count-pairs.js
+++ 53-InterviewPrep/ProblemSolvingPatterns/exercise/count-pairs.js
@@ -1,3 +1,14 @@
 // add whatever parameters you deem necessary
-function countPairs() {
+function countPairs(arr, num) {
+    let map = {}
+    let sums = 0
+    for(let element of arr){
+        if(map[num - element]){
+            sums++
+        }
+        map[element] = 1
+    }
+    return sums
 }
+
+module.exports = countPairs
