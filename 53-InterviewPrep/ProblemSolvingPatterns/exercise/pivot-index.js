55f1760231cf6626fbfa21d202583ae70c9a1680
AndrewSushi <AndrewSushi8@gmail.com>
2023-11-09 22:33:19 -0800

    Finished pivot index

--- 53-InterviewPrep/ProblemSolvingPatterns/exercise/pivot-index.js
+++ 53-InterviewPrep/ProblemSolvingPatterns/exercise/pivot-index.js
@@ -1,3 +1,18 @@
 // add whatever parameters you deem necessary
-function pivotIndex() {
+function pivotIndex(arr) {
+    let sum = 0
+    for(let num of arr){
+        sum += num
+    }
+
+    let leftSum = 0
+    for(let i = 0; i < arr.length; i++){
+        if(leftSum === sum - leftSum - arr[i]){
+            return i
+        }
+        leftSum += arr[i]
+    }
+    return -1
 }
+
+module.exports = pivotIndex
