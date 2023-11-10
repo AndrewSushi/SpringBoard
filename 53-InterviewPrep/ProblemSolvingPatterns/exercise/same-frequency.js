a0a6c7df6ad456cc9aef3c76e588113bda02f96a
AndrewSushi <AndrewSushi8@gmail.com>
2023-11-09 21:57:40 -0800

    Finished same frequency

--- 53-InterviewPrep/ProblemSolvingPatterns/exercise/same-frequency.js
+++ 53-InterviewPrep/ProblemSolvingPatterns/exercise/same-frequency.js
@@ -1,3 +1,22 @@
 // add whatever parameters you deem necessary
-function sameFrequency() {
+function sameFrequency(int1, int2) {
+    let frequency1 = {}
+    let frequency2 = {}
+
+    while(int1){
+        frequency1[int1 % 10] = (frequency1[int1 % 10] || 0) + 1
+        int1 = Math.floor(int1 / 10)
+    }
+    while(int2){
+        frequency2[int2 % 10] = (frequency2[int2 % 10] || 0) + 1
+        int2 = Math.floor(int2 / 10)
+    }
+    for(let key in frequency1){
+        if(frequency1[key] !== frequency2[key]){
+            return false
+        }
+    }
+    return true
 }
+
+module.exports = sameFrequency
